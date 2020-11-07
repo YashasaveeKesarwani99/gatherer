198b50815ea0d2ace298ee2dd7bbd787d7d6a9ca
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-11-07 19:23:31 +0530

    Initial commit

---
+++ Containers/Header/heading.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./heading.css";
+const Heading = () => {
+  return <div className="head">Gatherer</div>;
+};
+
+export default Heading;
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Heading from "../Containers/Header/heading";
+export default function App() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+}
