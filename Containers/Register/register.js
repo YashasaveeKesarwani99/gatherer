67dd81100bd7943fb1c8d84cf9bfbb6ee5128af6
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-12-16 19:50:51 +0530

    updated register container

--- Containers/Register/register.js
+++ Containers/Register/register.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
-
+import fireDb from "../../Components/firebase/firebase";
 class Register extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      event: []
+    };
+  }
+
+  componentDidMount() {
+    fireDb.ref("gatherer/makeAnEvent").on("value", (snapshot) => {
+      let event = [];
+      snapshot.forEach((snap) => {
+        event.push(snap.val());
+      });
+      this.setState({ event });
+    });
   }
 
   render() {
     return (
       <div>
-        <div className="head">Register In An Event</div>
+        <table>
+          <tr>
+            <th>s.No.</th>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Venue</th>
+            <th>Register</th>
+          </tr>
+          {this.state.event.map((obj) => {
+            let c = 1;
+            return (
+              <tr>
+                <th>{c}</th>
+                <th>{obj.eventName}</th>
+                <th>{obj.eventDate}</th>
+                <th>{obj.eventVenue}</th>
+                <th>Join</th>
+              </tr>
+            );
+          })}
+        </table>
       </div>
     );
   }
